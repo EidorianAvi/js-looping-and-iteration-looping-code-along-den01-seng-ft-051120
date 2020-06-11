44555be46462faf3fc1a149fe539e56bedb05ff7
Rene Avila <reneavila1993@gmail.com>
2020-06-11 15:57:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-// Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  let i = 0;
-  while(i < gifts.length){
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    i++
-  }
-  return gifts;
-}
-
-wrapGifts(gifts)
 
 function writeCards()
