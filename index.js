9b034c7f018ea2d56fca018c952451f3a1732309
Rene Avila <reneavila1993@gmail.com>
2020-06-11 16:09:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let names = ["Ada", "Brendan", "Ali"]
 
-function writeCards([])
+
+function writeCards(array, occasion){
+  for(let i = 0; i < array.length; i++){
+    console.log(`Thank you ${array[i]}, for the wonderful ${occasion} gift!`)
+  }
+}
