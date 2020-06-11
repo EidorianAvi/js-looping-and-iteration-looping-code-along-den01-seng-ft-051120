92a84cf65a9ac0168c25fe76ab83102a8ad0c4f0
Rene Avila <reneavila1993@gmail.com>
2020-06-11 16:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let names = ["Ada", "Brendan", "Ali"]
 function writeCards(array, occasion){
   let output =[];
   for(let i = 0; i < array.length; i++){
-    output.push(`Thank you ${array[i]}, for the wonderful ${occasion} gift!`)
+    output.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`)
   }
   
   return output;
